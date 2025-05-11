0d5da2c8a9be3fa5644043167acfdcb1079818ec
njiutheguru <stephennjiu@gmail.com>
2025-05-11 07:29:08 +0300

    deploy-4

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -10,12 +10,12 @@ const Hero = () => {
     >
       <div className="text-center">
         <h1
-          className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
+          className="text-2xl font-extrabold text-white sm:text-5xl md:text-6xl"
         >
-          Find The Perfect Rental
+          Find a Home Away From Home.
         </h1>
-        <p className="my-4 text-xl text-white">
-          Discover the perfect property that suits your needs.
+        <p className="my-4 text-xl text-white [font-family:cursive]">
+        Warmth & Style in Every Stay, in Every Keja.
         </p>
       </div>
       {/* <!-- Form Component --> */}
